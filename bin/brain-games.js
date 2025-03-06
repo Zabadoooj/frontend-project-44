15b1dca8790046de24a5050aa4a11f9c8f4bc747
Zabadoooj <qastuw@gmail.com>
2025-03-06 11:34:17 +0300

    upd

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,8 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
 console.log("Welcome to the Brain Games!")
+const name = readlineSync.question('Your answer: ');
+
+
+console.log(`Здарова, ${name}` )
