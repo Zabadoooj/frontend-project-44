eec176d683cb582442f11cfc2e9bcbead1b1d511
Zabadoooj <qastuw@gmail.com>
2025-03-11 14:31:38 +0300

    qweq

---
+++ bin/brain-even.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+import getUserName from '../src/cli.js';
+import readlineSync from 'readline-sync';
+
+
+console.log("Welcome to the Brain Games!");
+
+const username = getUserName();
+let gameCountes = 3;
+
+const game = () => {
+
+    console.log(`Answer "yes" if the number is even, otherwise answer "no".`);    
+    let randomNumb = Math.random() * 100;
+    randomNumb = parseInt(randomNumb);
+
+    console.log(`Question: ${randomNumb}`);
+
+    const answer = readlineSync.question(`Your answer: `);
+
+
+    if(answer == 'yes'){
+        if(randomNumb % 2 === 0){
+            console.log('Correct!');
+            
+        }else{
+            console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.`);
+            console.log(`Let's try again ${username}`);
+        }
+    }else if(answer == 'no'){
+        if(randomNumb % 2 !== 0){
+            console.log('Correct!');
+            
+        }else{
+            console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.`);
+            console.log(`Let's try again ${username}`);
+        }
+    }
+
+
+
+    gameCountes --;
+    chetnoyeGame();
+};
+
+const chetnoyeGame = () => {
+    
+    if (gameCountes > 0) {
+        game();
+    }else{
+        console.log(`Congratulations, ${username}`);
+    }
+}
+
+chetnoyeGame();
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,45 +1,6 @@
 #!/usr/bin/env node
-import getUserName from '../src/cli.js';
-import readlineSync from 'readline-sync';
+import getName from '../src/cli.js'
 
 
-console.log("Welcome to the Brain Games!");
-
-const username = getUserName();
-
-
-const chetnoyeGame = () => {
-
-    const randomNumb = Math.random() * 100;
-
-    console.log('Answer "yes" if the number is even, otherwise answer "no".')
-    console.log(`Question: ${parseInt(randomNumb)}`);
-    const answer = readlineSync.question("Your answer: ");
-    
-
-    if(answer === "yes"){
-        if(parseInt(randomNumb) % 2 === 0){
-            console.log("Correct!");
-
-        }else{
-            console.log("'yes' is wrong answer ;(. Correct answer was 'no'")
-            console.log(`Let's try again, ${username}`)
-
-        }
-        
-    }else if(answer === "no"){
-        if(parseInt(randomNumb) % 2 !== 0){
-            console.log("Correct!");
-
-        }else{
-            console.log("'yes' is wrong answer ;(. Correct answer was 'no'")
-            console.log(`Let's try again, ${username}`)
-
-        }
-
-    }else{
-        console.log(`Only "yes" or "no"`);
-        
-    }
-};
-chetnoyeGame()
+console.log("Welcome to the Brain Games!")
+console.log(`Здарова, ${getName()}`)
